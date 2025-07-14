ec5717c0f2d6a99217ade1513760b7dd3988a633
Mounis <mounisbhat@gmail.com>
2025-07-14 17:00:52 +0530

    feat: Implement TopKFrequent function to find the k most frequent elements and update main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/mounis-bhat/dsa-go/internal/neetcode150"
+import (
+	"fmt"
+
+	"github.com/mounis-bhat/dsa-go/internal/neetcode150"
+)
 
 func main() {
-	neetcode150.TopKFrequent([]int{1, 1, 1, 2, 2, 3}, 2)
+	res := neetcode150.TopKFrequent([]int{1, 1, 1, 2, 2, 3}, 2)
+	fmt.Println(res)
 }
--- internal/neetcode150/top_k_frequent.go
+++ internal/neetcode150/top_k_frequent.go
@@ -1,6 +1,8 @@
 package neetcode150
 
-import "fmt"
+import (
+	"container/heap"
+)
 
 // TopKFrequent returns the k most frequent elements from the given array.
 // The answer can be returned in any order.
@@ -23,9 +25,51 @@ import "fmt"
 // 4. Extract elements from the heap into the result
 //
 // Note: Go uses container/heap package which requires implementing heap.Interface
-func TopKFrequent(nums []int, k int) []int {
-	// TODO: Implement top k frequent elements logic
 
+// Implementing a heap
+
+// Struct to save the number and it's frequency
+type Node struct {
+	num  int
+	freq int
+}
+
+// FreqHeap is a slice of Freq structs.
+// Each Freq struct holds a number and its frequency.
+// Example visualization:
+//
+//	FreqHeap: [
+//	  {num: 1, freq: 3},
+//	  {num: 2, freq: 2},
+//	  {num: 3, freq: 1},
+//	]
+type FreqHeap []Node
+
+func (f FreqHeap) Len() int {
+	return len(f)
+}
+
+func (f FreqHeap) Less(i, j int) bool {
+	return f[i].freq > f[j].freq // Max heap: higher frequency has higher priority
+}
+
+func (f FreqHeap) Swap(i, j int) {
+	f[i], f[j] = f[j], f[i]
+}
+
+func (f *FreqHeap) Push(x any) {
+	*f = append(*f, x.(Node))
+}
+
+func (f *FreqHeap) Pop() any {
+	old := *f
+	n := len(old)
+	val := old[n-1]
+	*f = old[:n-1]
+	return val
+}
+
+func TopKFrequent(nums []int, k int) []int {
 	frequencyMap := make(map[int]int)
 
 	for _, v := range nums {
@@ -33,7 +77,19 @@ func TopKFrequent(nums []int, k int) []int {
 		frequencyMap[v] = count + 1
 	}
 
-	fmt.Println(frequencyMap)
+	h := &FreqHeap{}
+	heap.Init(h)
 
-	return nil
+	for num, freq := range frequencyMap {
+		heap.Push(h, Node{num: num, freq: freq})
+	}
+
+	topK := make([]int, 0, k)
+
+	for range k {
+		popped := heap.Pop(h).(Node)
+		topK = append(topK, popped.num)
+	}
+
+	return topK
 }
